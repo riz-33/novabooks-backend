4680e09dbec18c3e6c5f8ed72928e4ae399e1442
muhammad <rizwan.quettawala@gmail.com>
2026-03-06 16:48:19 +0500

    feat: add endpoint to retrieve user transactions

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -2,14 +2,22 @@ import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
 import {
   createTransaction,
-//   getTransactions,
-//   updateTransaction,
-//   deleteTransaction,
+  //   getTransactions,
+  //   updateTransaction,
+  //   deleteTransaction,
 } from "../controllers/transactionController.js";
 
 const router = express.Router();
 
 router.post("/", authMiddleware, createTransaction);
+router.get("/", authMiddleware, async (req, res) => {
+  const transactions = await Transaction.find({
+    userId: req.user.id,
+  }).populate("journalLines.accountId");
+
+  res.json(transactions);
+});
+
 // router.get("/", authMiddleware, getTransactions);
 // router.put("/:id", authMiddleware, updateTransaction);
 // router.delete("/:id", authMiddleware, deleteTransaction);
